2878bfc4b43af622ec9220a56dbb5292a01d5543
aistomin <andrej.istomin.ikeen@gmail.com>
2023-03-11 12:26:56 +0100

    Issue #1. Application skeleton.

---
+++ src/main/java/com/github/aistomin/deutsch/trainer/backend/DeutschTrainerBackendApplication.java
@@ -0,0 +1,10 @@
+package com.github.aistomin.deutsch.trainer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DeutschTrainerBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DeutschTrainerBackendApplication.class, args);
+	}
+}
